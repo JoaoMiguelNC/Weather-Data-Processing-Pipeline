beb5878f0e4644dce1746d1dae6ba30212983b67
JoaoMiguelNC <joao.nevescoelho@gmail.com>
2025-02-08 18:10:08 +0000

    small correction

--- main.tf
+++ main.tf
@@ -30,9 +30,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = google_cloud_run_service.default.project
-  service     = google_cloud_run_service.default.name
+  location    = google_cloud_run_service.weather-api.location
+  project     = google_cloud_run_service.weather-api.project
+  service     = google_cloud_run_service.weather-api.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
