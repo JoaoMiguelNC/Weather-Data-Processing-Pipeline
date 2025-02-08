7d15b1b82e0f976b9ac1f8c289e5d6e7a839e0aa
JoaoMiguelNC <joao.nevescoelho@gmail.com>
2025-02-08 16:40:55 +0000

    rename file

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ variable "project" {}
 resource "google_cloud_run_service" "weather-api" {
   name = "weather-api"
   location = "europe-west1"
-  project = "${project}"
+  project = "${var.project}"
   template {
     spec {
       containers {
-        image = "europe-west1-docker.pkg.dev${project}/cloud-run-containers/weather-api:latest"
+        image = "europe-west1-docker.pkg.dev${var.project}/cloud-run-containers/weather-api:latest"
       }
     }
   }
