6478223c17eae8fe193bd9dd30e936389f143dd3
JoaoMiguelNC <joao.nevescoelho@gmail.com>
2025-02-08 19:21:51 +0000

    destroy cloud run

--- main.tf
+++ main.tf
@@ -7,18 +7,18 @@ terraform {
 
 variable "project" {}
 
-resource "google_cloud_run_service" "weather-api" {
-  name = "weather-api"
-  location = "europe-west1"
-  project = "${var.project}"
-  template {
-    spec {
-      containers {
-        image = "europe-west1-docker.pkg.dev/${var.project}/cloud-run-containers/weather-api:latest"
-      }
-    }
-  }
-}
+# resource "google_cloud_run_service" "weather-api" {
+#   name = "weather-api"
+#   location = "europe-west1"
+#   project = "${var.project}"
+#   template {
+#     spec {
+#       containers {
+#         image = "europe-west1-docker.pkg.dev/${var.project}/cloud-run-containers/weather-api:latest"
+#       }
+#     }
+#   }
+# }
 
 data "google_iam_policy" "noauth" {
   binding {
@@ -29,10 +29,10 @@ data "google_iam_policy" "noauth" {
   }
 }
 
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.weather-api.location
-  project     = google_cloud_run_service.weather-api.project
-  service     = google_cloud_run_service.weather-api.name
+# resource "google_cloud_run_service_iam_policy" "noauth" {
+#   location    = google_cloud_run_service.weather-api.location
+#   project     = google_cloud_run_service.weather-api.project
+#   service     = google_cloud_run_service.weather-api.name
 
-  policy_data = data.google_iam_policy.noauth.policy_data
-}
+#   policy_data = data.google_iam_policy.noauth.policy_data
+# }
