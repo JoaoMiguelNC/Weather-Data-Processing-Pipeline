4f911abcd8c46fa50c59599eea2089bf2149ecce
JoaoMiguelNC <joao.nevescoelho@gmail.com>
2025-02-08 19:52:53 +0000

    small test

--- main.tf
+++ main.tf
@@ -8,22 +8,22 @@ terraform {
 variable "project" {}
 
 resource "google_cloud_run_v2_service" "weather-api" {
-  name = "weather-api"
-  location = "europe-west1"
-  project = "${var.project}"
-  template {
-  containers {
-    image = "europe-west1-docker.pkg.dev/${var.project}/cloud-run-containers/weather-api:latest"
-    env {
-        name = "WEATHER_KEY"
-        value_source {
-            secret_key_ref {
-                secret  = "open-weather-map-key"
-                version = "latest"
+    name = "weather-api"
+    location = "europe-west1"
+    project = "${var.project}"
+    template {
+        containers {
+            image = "europe-west1-docker.pkg.dev/${var.project}/cloud-run-containers/weather-api:latest"
+            env {
+                name = "WEATHER_KEY"
+                value_source {
+                    secret_key_ref {
+                        secret  = "open-weather-map-key"
+                        version = "latest"
+                    }
+                }
             }
         }
-    }
-    }
     }
 }
 
@@ -36,10 +36,10 @@ data "google_iam_policy" "noauth" {
   }
 }
 
-# resource "google_cloud_run_service_iam_policy" "noauth" {
-#   location    = google_cloud_run_v2_service.weather-api.location
-#   project     = google_cloud_run_v2_service.weather-api.project
-#   service     = google_cloud_run_v2_service.weather-api.name
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_v2_service.weather-api.location
+  project     = google_cloud_run_v2_service.weather-api.project
+  service     = google_cloud_run_v2_service.weather-api.name
 
-#   policy_data = data.google_iam_policy.noauth.policy_data
-# }
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
