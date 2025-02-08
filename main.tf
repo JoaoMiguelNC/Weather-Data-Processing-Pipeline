aefa52d85ceb9dc42d6a9e7015239059888a16ad
JoaoMiguelNC <joao.nevescoelho@gmail.com>
2025-02-08 16:31:52 +0000

    small change

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-# terraform {
-#   backend "gcs" {
-#     bucket = "weather-api-tfstate"
-#     prefix = "env/dev"
-#   }
-# }
-
-# This Terraform won't create any Google Cloud resources, so additional 
-# permissions are required for the service account
-#
 terraform {
   backend "gcs" {
     bucket = "weather-api-tfstate"
@@ -17,14 +7,13 @@ terraform {
 
 variable "project" {}
 
-# Cloud Run
 resource "google_cloud_run_service" "weather-api" {
   name = "weather-api"
   location = "europe-west1"
   template {
     spec {
       containers {
-        image = "europe-west1-docker.pkg.dev/${var.project}/cloud-run-containers/weather-api:latest"
+        image = "europe-west1-docker.pkg.dev/weather-api-code-challenge/cloud-run-containers/weather-api:latest"
       }
     }
   }
