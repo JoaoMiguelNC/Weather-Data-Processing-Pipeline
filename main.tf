acf197afe522163b9bdd9053c9392c3a5e20b3b7
JoaoMiguelNC <joao.nevescoelho@gmail.com>
2025-02-08 19:00:00 +0000

    small test

--- main.tf
+++ main.tf
@@ -7,18 +7,18 @@ terraform {
 
 variable "project" {}
 
-resource "google_cloud_run_service" "weather-api" {
-  name = "weather-api"
-  location = "europe-west1"
-  project = "${var.project}"
-  template {
-    spec {
-      containers {
-        image = "europe-west1-docker.pkg.dev/${var.project}/cloud-run-containers/weather-api:latest"
-      }
-    }
-  }
-}
+# resource "google_cloud_run_service" "weather-api" {
+#   name = "weather-api"
+#   location = "europe-west1"
+#   project = "${var.project}"
+#   template {
+#     spec {
+#       containers {
+#         image = "europe-west1-docker.pkg.dev/${var.project}/cloud-run-containers/weather-api:latest"
+#       }
+#     }
+#   }
+# }
 
 data "google_iam_policy" "noauth" {
   binding {
