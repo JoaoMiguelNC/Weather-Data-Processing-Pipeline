283259a474e069846b80c5a3eff5983cdb88977d
JoaoMiguelNC <joao.nevescoelho@gmail.com>
2025-02-08 13:36:40 +0000

    save terraform state

--- main.tf
+++ main.tf
@@ -8,12 +8,9 @@
 # This Terraform won't create any Google Cloud resources, so additional 
 # permissions are required for the service account
 #
-resource "random_string" "random" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
-}
-
-output "random_string" {
-    value = random_string.random.id
+terraform {
+  backend "gcs" {
+    bucket = "PROJECT_ID-tfstate"
+    prefix = "env/dev"
+  }
 }
