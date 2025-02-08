2b27ac3029d55243057f9baa07fe0f1fc305d830
JoaoMiguelNC <joao.nevescoelho@gmail.com>
2025-02-08 19:47:46 +0000

    small test

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ resource "google_cloud_run_v2_service" "weather-api" {
   name = "weather-api"
   location = "europe-west1"
   project = "${var.project}"
-  deletion_protection=false
   template {
   containers {
     image = "europe-west1-docker.pkg.dev/${var.project}/cloud-run-containers/weather-api:latest"
