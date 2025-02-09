17de04361eb2e44b8c5fb81f7f9d82ec18b12ad3
JoaoMiguelNC <joao.nevescoelho@gmail.com>
2025-02-09 07:07:35 +0000

    take down cloud run

--- app/main.py
+++ app/main.py
@@ -2,20 +2,16 @@ from flask import Flask
 import os
 import requests
 
-from google.cloud.secretmanager import SecretManagerServiceClient, AccessSecretVersionRequest
-
-# # Getting OpenWeatherMap key from Secret manager
-# secret_client: SecretManagerServiceClient = SecretManagerServiceClient()
-# secret_request: AccessSecretVersionRequest = AccessSecretVersionRequest(
-#     name='open-weather-map-key ')
-# key = secret_client.access_secret_version(
-#     request=secret_request).payload.data.decode('utf-8')
-
 key = os.getenv('WEATHER_KEY', 'no_value')
 
 app = Flask(__name__)
 
-list_cities = ['Lisbon']
+list_cities = ['Lisbon,PT', 'Porto,PT', 'London,GB', 'Munich,DE', 'Paris,FR']
+cities = {}
+for city in list_cities:
+    coordinates = requests.get(
+        f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={key}')
+    cities[city[:-3]] = (coordinates.json()[0]['lat'], coordinates.json()[0]['lon'])
 
 
 @app.route('/')
@@ -25,11 +21,9 @@ def index():
 
 @app.route('/weather/current/<city>')
 def get_current_temperature(city):
-    coordinates = requests.get(
-        f'http://api.openweathermap.org/geo/1.0/direct?q={city},PT&limit=1&appid={key}')
 
-    lat = coordinates.json()[0]['lat']
-    lon = coordinates.json()[0]['lon']
+    lat = cities[city][0]
+    lon = cities[city][1]
 
     exclude = 'minutely,hourly,daily,alerts'
     weather = requests.get(
@@ -41,6 +35,17 @@ def get_current_temperature(city):
     temperature['feels_like'] = weather.json()['current']['feels_like']
 
     return temperature
+
+# @app.route('/weather/stats/<city>')
+# def get_temperature_status(city):
+#     lat = cities[city][0]
+#     lon = cities[city][1]
+
+#     exclude = 'current,minutely,hourly,alerts'
+#     weather = requests.get(
+#         f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={exclude}&appid={key}&units=metric')
+
+#     return weather.json()
 
 if __name__ == "__main__":
     app.run()
